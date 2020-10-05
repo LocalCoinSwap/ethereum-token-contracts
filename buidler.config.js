3297b3c5fff3b7ce7737cdfaa41c08f8178561aa
codeforcer <nathancworsley@gmail.com>
2020-10-05 08:29:44 +0100

    add process.env

--- buidler.config.js
+++ buidler.config.js
@@ -1,12 +1,20 @@
+require('dotenv').config();
+
 usePlugin("@nomiclabs/buidler-ethers");
 usePlugin("@nomiclabs/buidler-truffle5");
 
 
 module.exports = {
+  defaultNetwork: "buidlerevm",
   networks: {
-    "buidlerevm": {
+    buidlerevm: {
       "chainId": 1,
-    }
+    },
+    // [BE CAREFUL THIS IS EXPENSIVE]
+    mainnet: {
+      url: process.env.WEB3_API,
+      accounts: [process.env.PRIV]
+    },
   },
   solc: {
     version: "0.5.17",
